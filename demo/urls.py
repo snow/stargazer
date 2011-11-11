788e4be1cd04be1141a10cbc4605e5385be8154d
Snow.Hellsing <snow.hellsing@gmail.com>
2011-11-11 22:53:52 +0800

    now use brower geo api to get address when creating post

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
-from demo.views import UnderConstructionView, IndexView, CreatePostView, LatLng2Addr
+from demo.views import UnderConstructionView, IndexView, CreatePostView,\
+                       LatLng2AddrView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
