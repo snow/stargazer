09781aa47e6830685331058746cf5c03503adfc4
Snow.Hellsing <snow.hellsing@gmail.com>
2011-11-27 16:09:04 +0800

    mf

--- settings_common.py
+++ settings_common.py
@@ -76,7 +76,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + '/webroot/s/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -92,7 +92,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '{}/webroot/s'.format(PROJECT_ROOT),
 )
 
 # List of finder classes that know how to find static files in
